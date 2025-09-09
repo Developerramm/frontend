b02251d78466496ef0c68d27ec85a8fc3f9380e9
ram kumar <rr697144@gmail.com>
2025-09-10 01:13:52 +0530

    adding jsx in reaact

--- Reactjs/project1/src/App.jsx
+++ Reactjs/project1/src/App.jsx
@@ -1,34 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  console.log("Ram kumar maniyari")
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>This is react js first tutorial here </h1>
+      <img src="/img1.png" alt="Queen of Tears" />
+      <h2>Name : Queen of Tears</h2>
+      <h3>Rating : 3</h3>
+      <p>Summery : Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias impedit possimus animi eligendi aliquam nesciunt necessitatibus magni nobis dignissimos laborum.</p>
+    </div>
   )
 }
 
