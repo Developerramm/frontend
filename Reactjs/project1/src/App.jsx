154c91340041fe312e9ce917a92b8ccc7a26a3bf
ram kumar <rr697144@gmail.com>
2025-09-10 02:07:31 +0530

    conditional rendering in react

--- Reactjs/project1/src/App.jsx
+++ Reactjs/project1/src/App.jsx
@@ -3,11 +3,18 @@ import NetflixSeries from './components/NetflixSeries'
 
 const App = () => {
 
+  let age = 30;
+  let username = "Ram kumar";
+  username = "";
   
 
   return (
     <React.Fragment>
       <h1>This is our react app</h1>
+
+      <h3> {age > 24 ? "Adult" : "Minor"} </h3>
+      <h3> {username || "Guest user"} </h3>
+
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
