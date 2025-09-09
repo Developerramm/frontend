6069b760896805302fa3769f0c366759777263d9
ram kumar <rr697144@gmail.com>
2025-09-10 01:26:09 +0530

    component

--- Reactjs/project1/src/App.jsx
+++ Reactjs/project1/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import NetflixSeries from './components/NetflixSeries'
 
 const App = () => {
-  console.log("Ram kumar maniyari")
+
   return (
     <div>
       <h1>This is react js first tutorial here </h1>
-      <img src="/img1.png" alt="Queen of Tears" />
-      <h2>Name : Queen of Tears</h2>
-      <h3>Rating : 3</h3>
-      <p>Summery : Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias impedit possimus animi eligendi aliquam nesciunt necessitatibus magni nobis dignissimos laborum.</p>
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
     </div>
   )
 }
---
+++ Reactjs/project1/src/components/NetflixSeries.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NetflixSeries = () => {
+  return (
+    <div>
+      <div>
+        <img src="/img1.png" alt="Queen of Tears" width="40%" height="40%" />
+      </div>
+      <h2>Name : Queen of Tears</h2>
+      <h3>Rating : 3</h3>
+      <p>
+        Summery : Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+        impedit possimus animi eligendi aliquam nesciunt necessitatibus magni
+        nobis dignissimos laborum.
+      </p>
+    </div>
+  );
+};
+
+export default NetflixSeries;
