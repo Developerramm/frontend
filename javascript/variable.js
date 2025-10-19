a942479a5f0484ba0890e6a0fae12804f1993956
ram kumar <rr697144@gmail.com>
2025-10-19 23:38:47 +0530

    variable in javascript

---
+++ javascript/variable.js
@@ -0,0 +1,15 @@
+let message;
+console.log(message)
+
+message = "welcome to this duniya";
+console.log(message)
+
+message = 100;
+console.log(message)
+
+// console.log(counter)
+
+const workingDay = 5;
+console.log(workingDay)
+
+// workingDay = 3;
