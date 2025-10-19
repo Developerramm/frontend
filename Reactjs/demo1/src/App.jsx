4d4f3834c3f34d9776cecf2893942274823ed793
ram kumar <rr697144@gmail.com>
2025-10-19 23:08:48 +0530

    component in react

---
+++ Reactjs/demo1/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NetFlixSeries from './components/NetFlixSeries'
+
+const App = () => {
+  return (
+    <div>
+      <NetFlixSeries />
+      <NetFlixSeries />
+      <NetFlixSeries />
+      <NetFlixSeries />
+      <NetFlixSeries />
+    </div>
+  )
+}
+
+export default App
---
+++ Reactjs/demo1/src/components/NetFlixSeries.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const NetFlixSeries = () => {
+  return (
+    <div>
+      <h3>netflix series card here </h3>
+
+      <div>
+        <img src="a1.jpg" alt="dark" />
+      </div>
+      <h2>Name : Dark</h2>
+      <h3>Rating : 8.3</h3>
+      <p>
+        Summery : Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Autem, modi. Saepe aliquid quidem reprehenderit sunt voluptate
+        consequuntur, debitis, perspiciatis eaque odio similique aut repellendus
+        explicabo voluptates itaque nobis enim fugiat quo adipisci molestiae
+        nihil ratione voluptatibus inventore. Quibusdam, ea cupiditate
+        doloremque porro, maxime, tenetur voluptatem voluptate dolorum quod
+        aspernatur perferendis.
+      </p>
+    </div>
+  );
+};
+
+export default NetFlixSeries;
