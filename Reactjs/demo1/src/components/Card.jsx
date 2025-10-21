f1d856f5103cb201b23389d549bc857d7b260a91
ram kumar <rr697144@gmail.com>
2025-10-21 21:35:31 +0530

    props in react js

--- Reactjs/demo1/src/components/Card.jsx
+++ Reactjs/demo1/src/components/Card.jsx
@@ -1,27 +1,16 @@
 import React from "react";
 import seriesData from "../api/seriesData.json";
+import List from "./List";
 
 const Card = () => {
-  console.log(seriesData);
+  
   return (
     <div>
       <div className="container">
         <div className="row">
           {seriesData.map((item) => {
             return (
-              <div className="col-md-4 my-4" key={item.id}>
-                <div className="card">
-                  <img src={item.img_url} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">{item.name}</h5>
-                    <h5 className="card-title"> Rating : {item.rating}</h5>
-                    <p className="card-text">{item.description}</p>
-                    <a href={item.watch_url} className="btn btn-primary">
-                      Watch Now
-                    </a>
-                  </div>
-                </div>
-              </div>
+              <List item = {item} key={item.id} />
             );
           })}
         </div>
