e620f5a137872032aa1703a83020c6c016c0b64c
ram kumar <rr697144@gmail.com>
2025-10-21 07:40:17 +0530

    dynamic value in react js

--- Reactjs/demo1/src/components/NetFlixSeries.jsx
+++ Reactjs/demo1/src/components/NetFlixSeries.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 
 const NetFlixSeries = () => {
-  return (
-    <div>
-      <h3>netflix series card here </h3>
 
-      <div>
-        <img src="a1.jpg" alt="dark" />
-      </div>
-      <h2>Name : Dark</h2>
-      <h3>Rating : 8.3</h3>
-      <p>
-        Summery : Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+  let name = "Queen of tears";
+  let rating = 3.2;
+  let summery = `Lorem ipsum dolor sit amet consectetur, adipisicing elit.
         Autem, modi. Saepe aliquid quidem reprehenderit sunt voluptate
         consequuntur, debitis, perspiciatis eaque odio similique aut repellendus
         explicabo voluptates itaque nobis enim fugiat quo adipisci molestiae
         nihil ratione voluptatibus inventore. Quibusdam, ea cupiditate
         doloremque porro, maxime, tenetur voluptatem voluptate dolorum quod
-        aspernatur perferendis.
+        aspernatur perferendis`;
+
+  return (
+    <div>
+      <h3>netflix series card here </h3>
+
+      <h1> {3 + 2 + 4 + 10 - 3} </h1>
+
+      <div>
+        <img src="a1.jpg" alt="dark" />
+      </div>
+      <h2>Name : {name} </h2>
+      <h3>Rating : {rating} </h3>
+      <p>
+        Summery : {summery}
       </p>
     </div>
   );
