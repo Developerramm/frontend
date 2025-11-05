3dc3a34263a1c8956ca8ccbeb010f3109928d7b7
ram kumar <rr697144@gmail.com>
2025-11-05 16:37:44 +0530

    optional chaining operator in java

--- javascript/obj2.js
+++ javascript/obj2.js
@@ -25,3 +25,36 @@ let {
 console.log(firstName);
 console.log(lastName);
 console.log(name);
+
+console.log("*******************************************");
+
+function getUser(id) {
+  if (id <= 0) {
+    return null;
+  }
+
+  return {
+    id: id,
+    username: "Ram kumar",
+    profile: {
+      avatar: "avatar.jpb",
+      language: "Hindi",
+    },
+  };
+}
+
+let user = getUser(1);
+console.log(user);
+
+let profile = user && user.profile;
+console.log(profile);
+
+let p1 = user?.profile;
+console.log(p1)
+
+
+let user1 = getUser(0);
+// console.log(user1.profile)
+
+console.log(user1?.profile)
+console.log(user1 && profile)
