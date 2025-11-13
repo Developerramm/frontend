1d63440cdebd983b3227d69a7a8a976a80254cb8
ram kumar <rr697144@gmail.com>
2025-11-13 12:54:10 +0530

    jsx in react js

--- Reactjs/demo1/src/App.jsx
+++ Reactjs/demo1/src/App.jsx
@@ -1,46 +1,14 @@
-<<<<<<< HEAD
-import React from "react";
-import JsxDemo from "./components/JsxDemo";
+import React from 'react'
+import JsxDemo from './components/JsxDemo'
+import Bye from './components/Bye'
 
 const App = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <h3>React js demo tutorial here below</h3>
-        <JsxDemo />
-      </div>
-=======
-import Card from "./components/Card";
-import ProfileCard from "./components/ProfileCard";
-
-const App = () => {
-
-  return (
-    <div>
-
-    <Card />
-
-
-      <ProfileCard
-
-        name = "ram kumar"
-        age = {25}
-        greeting = {
-          <div>
-            <strong>Hello ram, keep up the great work </strong>
-          </div>
-        }
-      
-      >
-    
-        <p>Hobbies traveling foodie</p>
-        <button>Contact me </button>
-
-
-      </ProfileCard>
->>>>>>> d4554a1ed4a659ad5179f49cccd2e444cebe375c
+    <div className='container'>
+    <JsxDemo />
+    <Bye />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- Reactjs/demo1/src/components/JsxDemo.jsx
+++ Reactjs/demo1/src/components/JsxDemo.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const JsxDemo = () => {
+    let name = "Ram kumar maniyari";
   return (
     <div className="row">
       <div className="col-md-6">
         <h3>Jsx demo tutorial here below </h3>
+        <img src="a1.jpg" alt="" />
+        <h3>Your name is {name} </h3>
       </div>
     </div>
   );
