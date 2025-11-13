fb9418ce476a127d4020b99afbc274e0407ad51a
ram kumar <rr697144@gmail.com>
2025-11-13 12:29:58 +0530

    react tutorial

---
+++ Reactjs/demo1/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import JsxDemo from "./components/JsxDemo";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <h3>React js demo tutorial here below</h3>
+        <JsxDemo />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ Reactjs/demo1/src/components/JsxDemo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const JsxDemo = () => {
+  return (
+    <div className="row">
+      <div className="col-md-6">
+        <h3>Jsx demo tutorial here below </h3>
+      </div>
+    </div>
+  );
+};
+
+export default JsxDemo;
